c0104183228841222bfdc75fc422021b520a1b7c
qianyucc <1413979079@qq.com>
2019-05-27 22:24:03 +0800

    add bootstrap and index.html

---
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -0,0 +1,17 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author lijing
+ * @date 2019-05-26-22:26
+ * @discroption
+ */
+@Controller
+public class IndexController {
+    @GetMapping(name = "/index")
+    public String index() {
+        return "index";
+    }
+}
